556752fc2d69740d8a1a927cdcca8ac1400d2551
chenmo <2010557767@qq.com>
2019-07-25 18:26:54 +0800

    加个readme.md

---
+++ readme.md
@@ -0,0 +1,5 @@
+# 简单的的springboot和mybatisplus后端操作
+
+> 使用方法 
+>
+> git clone https://github.com/chenmoand/SpringBootBlog.git 
--- src/main/java/com/brageast/blog/mapper/ArticleMapper.java
+++ src/main/java/com/brageast/blog/mapper/ArticleMapper.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.brageast.blog.entity.Article;
 import com.brageast.blog.entity.BaseArticle;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.Date;
 
@@ -14,8 +13,7 @@ public interface ArticleMapper extends BaseMapper<Article> {
     void addArtice(Long id, String articleName, String describe,
                    String author, String lable, String classification,
                    String content, Date date, String url);
-    void updateArtice(@Param("id") Long id, @Param("articleName") String articleName,
-                      @Param("describe") String describe, @Param("author") String author,
-                      @Param("lable") String lable, @Param("classification") String classification,
-                      @Param("content") String content, @Param("date") Date date, @Param("url") String url);
+    void updateArtice(Long id, String articleName, String describe,
+                      String author, String lable, String classification,
+                      String content, Date date, String url);
 }
